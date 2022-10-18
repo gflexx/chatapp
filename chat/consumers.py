b6d59279c74a7dee408da1b9420bb8543bd217cd
gflexx <glennenri24@gmail.com>
2022-10-18 10:01:59 +0300

    updated models and consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,32 +1,81 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
+from django.contrib.auth import get_user_model
+from asgiref.sync import async_to_sync
+from django.core import serializers
 import json
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+from .models import Message
+
+Users = get_user_model()
+
+
+class ChatConsumer(WebsocketConsumer):
+    def get_messages(self, data):
+        msgs = Message.get_last_10_msgs()
+        content = {
+            'messages': self.messages_to_json(msgs)
+        }
+        self.send_message(content)
+
+    def new_message(self, data):
+        owner = data['owner']
+        user = Users.objects.filter(id=owner)[0]
+        msg = Message.objects.create(
+            owner=user,
+            text=data['text']
+        )
+        content = {
+            'message': self.serialize_msg(msg)
+        }
+        
+
+
+    def messages_to_json(self, msgs):
+        results = []
+        for msg in msgs:
+            results.append(self.serialize_msg(msg))
+        return results
+
+    def serialize_msg(self, msg):
+        data = {
+            'author': msg.owner.username,
+            'text': msg.text,
+            'created_at': str(msgs.created_at)
+        }
+        return data
+
+    commands = {
+        'get_messages': get_messages,
+        'new_message': new_message
+    }
+
+    def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
 
         # join room group
-        await self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
     # receive msg from websocket
-    async def receive(self, text_data):
-        json_text = json.loads(text_data)
-        msg = json_text['message']
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        msg = data['message']
+        self.commands[data['command']](self, data)
 
-        # send msg to group room
-        await self.channel_layer.group_send(
+    # send msg to group room
+    def send_chat_message(self, msg):
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -34,13 +83,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-        
-    async def chat_message(self, event):
-        msg = event['message']
+    def send_message(self, msg):
+        self.send(text_data=json.dumps(msg)) 
 
-        # send msg to websocket
-        await self.send(
-            text_data=json.dumps({
-                'message': msg,
-            })
-        )
+    def chat_message(self, event):
+        msg = event['message']
+        self.send(text_data=json.dumps(msg))
+        
--- chat/models.py
+++ chat/models.py
@@ -1,8 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users', default='default.jpg')
     
     def __str__(self):
         return self.username
+
+class Message(models.Model):
+    owner = models.ForeignKey(
+        get_user_model(), 
+        on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.owner.username
+
+    def get_last_10_msgs(self):
+        return Message.objects.order_by('-created_at').all[10]
